ffbf57bd07052dd2fa48de8d0c91b6ff15fce094
Sungyong An <sungyong.an@webtoonscorp.com>
2026-03-20 22:33:20 +0900

    Declare tasks as not compatible with configuration cache
    Lambda properties (allowedFilter, baselineMap) cannot be serialized
    by the configuration cache. Mark tasks explicitly to give users a
    clear error message instead of a cryptic serialization failure.

--- manifest-guard/src/main/kotlin/io/github/fornewid/gradle/plugins/manifestguard/internal/utils/Tasks.kt
+++ manifest-guard/src/main/kotlin/io/github/fornewid/gradle/plugins/manifestguard/internal/utils/Tasks.kt
@@ -2,9 +2,9 @@ package io.github.fornewid.gradle.plugins.manifestguard.internal.utils
 
 import org.gradle.api.Task
 
-@Suppress("UnstableApiUsage")
 internal object Tasks {
     fun Task.declareCompatibilities() {
         doNotTrackState("This task only outputs to console")
+        notCompatibleWithConfigurationCache("Lambda properties (allowedFilter, baselineMap) are not serializable")
     }
 }
